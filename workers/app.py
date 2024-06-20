6371baf6a130d0057a9bf410fd3820a719c27f94
Yutahhhhh <yutahhhh@theblueback.com>
2024-06-20 22:00:39 +0900

    fix/lint

--- workers/app.py
+++ workers/app.py
@@ -2,6 +2,7 @@
 This module configures the Flask application and its routes.
 """
 
+import os
 import sys
 
 from flask import Flask
@@ -11,7 +12,6 @@ from blueprints.genre import genre_bp
 from blueprints.lyric import lyric_bp
 from config import Config
 from services.log_utils import setup_logging
-import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
